86f32672508131ea2f10a373a1d03cb8bfa11e86
Collin Snyder <collinjacobsnyder@gmail.com>
2020-04-14 16:03:20 -0500

    Update server

--- server/index.js
+++ server/index.js
@@ -3,16 +3,3 @@ const path = require("path");
 const cors = require("cors");
 const port = process.env.PORT || 3000;
 
-const app = express();
-
-app.use(express.static(path.join(__dirname + "/../client/dist/")));
-app.use(express.json());
-app.use(cors());
-
-app.get("/", (req, res) => {
-    res.send("Welcome to your Express-React app!");
-})
-
-app.listen(port, () => {
-  console.log(`Express server listening on port ${port}!`);
-});
