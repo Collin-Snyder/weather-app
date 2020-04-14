3ea115fab085403455960eb75521931b695d0c72
Collin Snyder <collinjacobsnyder@gmail.com>
2020-04-14 14:40:50 -0500

    Add file loader to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,30 @@
 module.exports = {
-    entry: __dirname + '/client/src/index.jsx',
-    module: {
-      rules: [
-        {
-          test: [/\.jsx$|\.js$/],
-          exclude: /node_modules/,
-          use: {
-            loader: 'babel-loader',
-            options: {
-              presets: ['@babel/preset-react', '@babel/preset-env']
-            }
-          }
+  entry: __dirname + "/client/src/index.jsx",
+  module: {
+    rules: [
+      {
+        test: [/\.jsx$|\.js$|\.html$/],
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-react", "@babel/preset-env"],
+          },
         },
-        {
-          test: [/\.css$|\.scss$|\.sass$/],
-          exclude: /node_modules/,
-          use: ['style-loader', 'css-loader']
-        }
-      ]
-    },
-     output: {
-      filename: 'bundle.js',
-      path: __dirname + '/client/dist'
-    }
-  };
+      },
+      {
+        test: [/\.css$|\.scss$|\.sass$/],
+        exclude: /node_modules/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: [/\.(png|jpe?g|gif)$/i],
+        use: ["file-loader"],
+      },
+    ],
+  },
+  output: {
+    filename: "bundle.js",
+    path: __dirname + "/client/dist",
+  },
+};
